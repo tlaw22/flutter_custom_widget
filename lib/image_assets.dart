4d08cf538bf46c32e8082d7f743f3de677246a41
Tim Lawless <108081811+tlaw22@users.noreply.github.com>
2024-02-14 11:57:38 -0500

    lesson complete

--- lib/image_assets.dart
+++ lib/image_assets.dart
@@ -12,32 +12,50 @@ class _ImageAssetsWidgetState extends State<ImageAssetsWidget> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-          child: Column(
-        children: [
-          Container(
-            width: 300,
-            height: 200,
-            color: Colors.yellowAccent,
-            child: Image.asset(
-              'lib/assets/images/flat-icons2.jpg',
-              fit: BoxFit.contain,
-              filterQuality: FilterQuality.high,
-            ),
-          ),
-          Container(
-            width: 300,
-            height: 200,
-            color: Colors.blueAccent,
-            child: GestureDetector(onTap: (){
-              
-            },
-              child: Image.asset(
-                'lib/assets/images/flat-icons.jpg',
-                fit: BoxFit.contain,
+          child: IntrinsicHeight(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FadeInImage.assetNetwork(
+                placeholder: 'lib/assets/images/smile.png',
+                image:
+                    'https://th.bing.com/th/id/R.a1c9a9179e65afdcf84fbc9afd8e9218?rik=4hgNN2Rd9wrv8w&riu=http%3a%2f%2fkerrisdaledental.com%2fwp-content%2fuploads%2f2016%2f11%2fsmile.jpg&ehk=t9lVeG5fMIfivZDTWv19cRYKHaFe0mckZBXhPQhm5GU%3d&risl=&pid=ImgRaw&r=0'),
+            Expanded(
+              child: Container(
+                height: 200,
+                color: Colors.yellowAccent,
+                child: Image.network(
+                  'https://th.bing.com/th/id/R.a1c9a9179e65afdcf84fbc9afd8e9218?rik=4hgNN2Rd9wrv8w&riu=http%3a%2f%2fkerrisdaledental.com%2fwp-content%2fuploads%2f2016%2f11%2fsmile.jpg&ehk=t9lVeG5fMIfivZDTWv19cRYKHaFe0mckZBXhPQhm5GU%3d&risl=&pid=ImgRaw&r=0',
+                  fit: BoxFit.contain,
+                ),
               ),
             ),
-          )
-        ],
+            Expanded(
+              child: Container(
+                color: Colors.blueAccent,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Image.asset(
+                    'lib/assets/images/flat-icons.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.blueAccent,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Image.asset(
+                    'lib/assets/images/flat-icons.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
